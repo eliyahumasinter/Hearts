455e8c6812ec916d0b4a96b73688a9243509a41a
Eliyahu Masinter <eliyahumasinter@gmail.com>
2024-06-09 00:52:05 +0300

    Ensure a game starts with 4 unique players

--- game.py
+++ game.py
@@ -2,8 +2,23 @@ from player import Player
 from deck import Deck
 
 
+class BadPlayerListError(Exception):
+    pass
+
+
 class Game:
     def __init__(self, players: list[Player]):
+        """Initialize the game with the given players and deal the cards. Ensure that there are a correct number of unique players
+        Version 1.0 - Only supports 4 players
+
+        Args:
+            players (list[Player]): A list of players
+        """
+
+        if len(players) != 4 or len([player.name.lower() for player in players]) != 4:
+            raise BadPlayerListError(
+                "The game only supports 4 players. Please provide a list of 4 players with unique names.")
+
         self.players = players
         self.deck = Deck()
         self.hands = self.deck.deal()
