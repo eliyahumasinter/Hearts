5ef0f220d7ce5ebe157bb71b42d3faf89104d7fc
Eliyahu Masinter <eliyahumasinter@gmail.com>
2024-06-09 00:51:15 +0300

    Card class tests

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -27,6 +27,45 @@ class DeckTests(unittest.TestCase):
         self.assertEqual(len(set([str(card) for hand in hands for card in hand])),
                          52, "All cards should be dealt exactly once")
 
+    def test_sorting(self):
+        deck = Deck()
+        deck_cards = [card.short_name() for card in deck.cards]
+        deck.shuffle()
+        sorted_deck = Deck.sort_hand(deck.cards)
+        sorted_deck_cards = [card.short_name() for card in sorted_deck]
+        self.assertEqual(deck_cards, sorted_deck_cards,
+                         "The sorted deck should be the same as the original deck")
+
+
+class CardTests(unittest.TestCase):
+
+    def test_points(self):
+        deck = Deck()
+        for card in deck.cards:
+            if card.suit == "hearts":
+                self.assertEqual(card.points(), 1,
+                                 f"The {card} should be worth 1 point")
+            elif card.suit == "diamonds" and card.rank == 11:
+                self.assertEqual(card.points(), -10,
+                                 f"The {card} should be worth -10 points")
+            elif card.suit == "spades" and card.rank == 12:
+                self.assertEqual(card.points(), 13,
+                                 f"The {card} should be worth 13 points")
+            else:
+                self.assertEqual(card.points(), 0,
+                                 f"The {card} should be worth 0 points")
+
+    def test_str(self):
+        card = Deck.Card("clubs", "10")  # 10 of clubs
+        self.assertEqual(str(card), "10 of clubs",
+                         "The string representation of the card is incorrect")
+        card = Deck.Card("hearts", 14)  # Ace of hearts
+        self.assertEqual(str(card), "Ace of hearts",
+                         "The string representation of the card is incorrect")
+        card = Deck.Card("diamonds", 11)  # Jack of diamonds
+        self.assertEqual(str(card), "Jack of diamonds",
+                         "The string representation of the card is incorrect")
+
 
 if __name__ == '__main__':
     unittest.main()
