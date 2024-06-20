12d426c11ed5d76793a248184000595b7151cb36
Eliyahu Masinter <eliyahumasinter@gmail.com>
2024-06-20 05:23:55 +0300

    Implement multi threading to allow concurrent games, and a splash of color

--- server.py
+++ server.py
@@ -5,9 +5,52 @@ from api import API
 from backend.player import Player
 from backend.deck import Deck, SUIT
 from backend.round import Round
+import threading
 
 SERVER_PORT = 2345
 SERVER_IP = "0.0.0.0"
+
+
+class Print:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    @staticmethod
+    def print(*args, color=None, bold=False, underline=False, end='\n', sep=' ', encode=True):
+
+        string = ''
+        if color:
+            string += color
+        if bold:
+            string += Print.BOLD
+        if underline:
+            string += Print.UNDERLINE
+        for arg in args:
+            string += str(arg) + sep
+        string += Print.END + end
+
+        if encode:
+            return string.encode()
+        return string
+
+    def clear(cls):
+        return "\033[H\033[J".encode()
+
+    def display_hand(cls, hand: list[Deck.Card]):
+        string = ""
+        for index, card in enumerate(hand):
+            string += f"{index}: {card}\n"
+        return cls.print(string, color=cls.BLUE)
+
+    def __call__(cls, *args, **kwargs):
+        return cls.print(*args, **kwargs)
 
 
 def print_client_sockets(client_sockets):
@@ -25,27 +68,11 @@ def socket_to_name(clients_names, current_socket):
     return sender_name
 
 
-class Print:
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    END = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-    @staticmethod
-    def print(*args, color=None, bold=False, underline=False, end='\n'):
-        if color:
-            print(color, end='')
-        if bold:
-            print(Print.BOLD, end='')
-        if underline:
-            print(Print.UNDERLINE, end='')
-        print(*args, end='')
-        print(Print.END, end=end)
+class ThreadError(Exception):
+    def __init__(self, message, error, thread):
+        super().__init__(message)
+        self.error = error
+        self.thread = thread
 
 
 def main():
@@ -54,140 +81,226 @@ def main():
     server_socket.bind((SERVER_IP, SERVER_PORT))
     server_socket.listen()
     print("Listening for clients...")
-    staging_room_players = []
+    staging_room_players = {}
+    client_threading_map = {}
+    all_players = {}
     games = []
-    players = {}
-    # The server will wait for 4 players to connect and then it will start a game
+    NUM_PLAYERS = 1
+
+    def close_on_error(thread: Optional[threading.Thread] = None):
+        game = all_players[current_socket]
+        if game:
+            for player in game.players:
+                if game.players[player] != current_socket:
+                    game.players[player].send(Game.printer(
+                        "A player has left the game, destroying game", color=Game.printer.FAIL))
+                    game.players[player].close()
+                    del all_players[player]
+
+            games.remove(thread)  # type: ignore
+
+        current_socket.close()
+        del all_players[current_socket]
+        client_sockets.remove(current_socket)
+
     while True:
-        client_sockets, _, _ = select.select(
-            [server_socket] + staging_room_players, [], [])
-        for current_socket in client_sockets:
-            if current_socket == server_socket:
-                client_socket, _ = server_socket.accept()
-                print("New client connected")
-                staging_room_players.append(client_socket)
-            else:
-                data = current_socket.recv(1024).decode()
-                if data.startswith("NAME: "):
-                    name = data.split(":")[1].strip().capitalize()
-                    if name in players:
-                        current_socket.send("Name already taken".encode())
-                        continue
-                    players[name] = current_socket
-                    for player in staging_room_players:
-                        if player != current_socket:
-                            player.send(f"{name} has joined".encode())
-                        else:
-                            player.send(f"Welcome, {name}".encode())
-                elif data == 'EXIT':
-                    current_socket.close()
-                    staging_room_players.remove(current_socket)
-                    continue
-                if not data:
-                    print("Client disconnected")
-                    current_socket.close()
-                    staging_room_players.remove(current_socket)
+        try:
+            client_sockets, _, _ = select.select(
+                [server_socket] + list(all_players.keys()), [], [])
+            for current_socket in client_sockets:
+                if current_socket == server_socket:
+                    client_socket, _ = server_socket.accept()
+                    print("New client connected")
+                    all_players[client_socket] = None
                 else:
-                    print("Received data from client")
-                    print(data)
-                    # for client in staging_room_players:
-                    #     if client != current_socket:
-                    #         client.sendall(data.encode())
-
-        if len(players) == 1:
-            game = API()
-            games.append(game)
-            for player in players:
-                game.add_player(player)
-                players[player].send("Game starting!".encode())
-            staging_room_players = []
-
-            def hearts_broken_hook():
-                for player in players:
-                    players[player].send("Hearts has been broken!\n".encode())
-
-            def round_end_hook():
-                for user in players:
-                    players[user].send("Round has ended!\n".encode())
-                    players[user].send("Current scores: \n".encode())
-                    state = game.get_current_state()
-                    player_state = state['players']
-                    for player in player_state:
-                        players[user].send(
-                            f"\n\n{player}: {player_state[player]['total_score']}\n\n".encode())
-
-            def play_card_hook(player: Player, led_suit: Optional[SUIT], is_leading: bool) -> Deck.Card:
-                """Method to get the card to play from the player. This method will be called for each player in the trick. 
-
-                    Args:
-                        player (Player): The player whose turn it is to play
-                        led_suit (Optional[SUIT]): The suit that was led in the trick or None if the player is leading (except the first round where it is clubs)
-                        is_leading (bool): True if the player is leading the trick, False otherwise
-
-                    Returns:
-                        Deck.Card: The validated card the player wants to play
-                    """
-                player_socket = players[player.name]
-                # Get the cards that the player is allowed to play
-                allowed_cards = game.get_allowed_cards(
-                    player, led_suit, is_leading)
-                player_socket.send("Your turn to play\n".encode())
-                player_socket.send(
-                    f"Allowed cards: {dict(enumerate(allowed_cards))}\n\n\n".encode())
-                player_socket.send("INPUT".encode())
-
-                card = player_socket.recv(1024).decode().strip()
-
-                return allowed_cards[int(card)]
-
-            def get_pass_cards_hook(player: Player) -> list[Deck.Card]:
-                """Method to get the cards to pass from the player. This method will be called for each player at the beginning of the round."""
-                player_socket = players[player.name]
-                player_socket.send(
-                    f'Passing {game.get_passing_direction()}'.encode())
-                player_state = game.get_player_state(player)
-                player_hand = player_state['hand']
-
-                chosen_cards = []
-                for i in range(3):
-                    player_socket.send(
-                        f"Hand: {dict(enumerate(player_hand))}\n".encode())
-                    player_socket.send(
-                        "Enter the card to pass by number in list: \n".encode())
-                    player_socket.send("INPUT".encode())
-                    card_index = player_socket.recv(1024).decode().strip()
-                    if card_index.isdigit() and int(card_index) < len(player_hand):
-                        card_index = int(card_index)
-
-                    else:
-                        player_socket.send("Invalid input\n".encode())
-                        i -= 1
+                    try:
+                        data = current_socket.recv(1024).decode()
+                    except ConnectionError:
+                        print("Client disconnected")
+                        # Get the thread that the client is in
+                        thread = client_threading_map.get(current_socket)
+                        close_on_error(thread)
                         continue
-                    chosen_cards.append(player_hand[card_index])
-                    player_hand.pop(card_index)
-                return chosen_cards
+                    if data.startswith("NAME: "):
+                        name = data.split(":")[1].strip().capitalize()
+                        if name in staging_room_players:
+                            current_socket.send(Game.printer(
+                                "Name already taken", color=Game.printer.Warning))
+                            continue
+                        staging_room_players[name] = current_socket
+                        for player in staging_room_players.values():
+                            if player != current_socket:
+                                player.send(Game.printer(
+                                    f"{name} has joined", color=Game.printer.BLUE))
+                            else:
+                                player.send(Game.printer(
+                                    f"Welcome, {name}", color=Game.printer.BLUE))
+                    elif data == 'EXIT':
+                        current_socket.close()
+                        del all_players[current_socket]
+                        continue
+                    if not data:
+                        print("Client disconnected")
+                        # Get the thread that the client is in
+                        thread = client_threading_map.get(current_socket)
+                        close_on_error(thread)
+                    else:
+                        print("Received data from client\n", data)
 
-            def trick_end_hook(trick: 'Round.Trick') -> None:
-                trick_outcome = str(trick)
+            def thread_target_wrapper(players, shared_data):
+                try:
+                    create_game(players)
+                except Exception as e:
+                    shared_data['exception'] = ThreadError(
+                        "Error in game thread", e, threading.current_thread()
+                    )
+
+            def create_game(players):
+                game = Game(players)  # dict forces copy
                 for player in players:
-                    players[player].send(
-                        f"\n{'-'*10}\n\n{trick_outcome}\n\n{'-'*10}\n".encode())
+                    players[player].send(Game.printer.clear())
+                    players[player].send(Game.printer(
+                        "Game starting!\n", color=Game.printer.GREEN))
+                    all_players[players[player]] = game
 
-            def card_played_hook(player: Player, card: Deck.Card) -> None:
-                for p in players:
-                    if p != player.name:
-                        players[p].send(
-                            f"{player.name}: {card}\n".encode())
+                game.api.start_game()
+            if NUM_PLAYERS == len(staging_room_players):
+                shared_data = {}
+                game_thread = threading.Thread(target=thread_target_wrapper, args=(
+                    dict(staging_room_players), shared_data))  # dict forces copy
+                games.append(game_thread)
+                for player in staging_room_players:
+                    client_threading_map[staging_room_players[player]
+                                         ] = game_thread
 
-            game.set_play_card_hook(play_card_hook)
-            game.set_get_pass_cards_hook(get_pass_cards_hook)
+                staging_room_players.clear()
+                game_thread.start()
 
-            game.set_hearts_broken_hook(hearts_broken_hook)
-            game.set_round_end_hook(round_end_hook)
-            game.set_trick_end_hook(trick_end_hook)
-            game.set_card_played_hook(card_played_hook)
-            game.start_game()
+                if 'exception' in shared_data:
+                    raise shared_data['exception']
+        except ThreadError as e:
+            e.thread.join()
+            if not issubclass(type(e.error), (ConnectionError)):
+                raise e.error
+            close_on_error(e.thread)
+
+
+class Game:
+    printer = Print()
+
+    def __init__(self, players: dict[str, socket.socket]):
+        self.api = API()
+
+        self.players = players
+        for player in self.players:
+            self.api.add_player(player)
+
+        self.api.set_play_card_hook(self.play_card_hook)
+        self.api.set_get_pass_cards_hook(self.get_pass_cards_hook)
+        self.api.set_hearts_broken_hook(self.hearts_broken_hook)
+        self.api.set_round_end_hook(self.round_end_hook)
+        self.api.set_trick_end_hook(self.trick_end_hook)
+        self.api.set_card_played_hook(self.card_played_hook)
+
+    def round_end_hook(self):
+        for user in self.players:
+            self.players[user].send(Game.printer.clear())
+            self.players[user].send(Game.printer(
+                "Round has ended!\n", bold=True, color=Game.printer.GREEN))
+            self.players[user].send(Game.printer(
+                "\n\nScores:\n", bold=True, color=Game.printer.CYAN))
+            state = self.api.get_current_state()
+            player_state = state['players']
+            for player in player_state:
+                self.players[user].send(
+                    Game.printer(f"{player}: {player_state[player]['total_score']}", color=Game.printer.CYAN))
+
+    def play_card_hook(self, player: Player, led_suit: Optional[SUIT], is_leading: bool) -> Deck.Card:
+        """Method to get the card to play from the player. This method will be called for each player in the trick.
+
+            Args:
+                player (Player): The player whose turn it is to play
+                led_suit (Optional[SUIT]): The suit that was led in the trick or None if the player is leading (except the first round where it is clubs)
+                is_leading (bool): True if the player is leading the trick, False otherwise
+
+            Returns:
+                Deck.Card: The validated card the player wants to play
+            """
+        player_socket = self.players[player.name]
+        # Get the cards that the player is allowed to play
+        allowed_cards = self.api.get_allowed_cards(
+            player, led_suit, is_leading)
+        player_socket.send(Game.printer(
+            "Your turn to play", color=Game.printer.GREEN))
+        player_socket.send(Game.printer(
+            "Your hand:", color=Game.printer.HEADER))
+        player_socket.send(Game.printer.display_hand(allowed_cards))
+
+        return allowed_cards[self.get_valid_user_input(
+            player_socket, "Enter the card to play by number in list: ", len(allowed_cards))]
+
+    def get_valid_user_input(self, player_socket: socket.socket, message: str, upper_bound: int) -> int:
+        player_socket.send(Game.printer(message, color=Game.printer.CYAN))
+        player_socket.send("INPUT".encode())
+        card = player_socket.recv(1024).decode().strip()
+        if card.isdigit() and int(card) < upper_bound:
+            return int(card)
+        else:
+            while True:
+                player_socket.send(
+                    Game.printer("Invalid input, enter a number in range: ", color=Game.printer.FAIL))
+                player_socket.send("INPUT".encode())
+                card = player_socket.recv(1024).decode().strip()
+                if card.isdigit() and int(card) < upper_bound:
+                    return int(card)
+
+    def get_pass_cards_hook(self, player: Player) -> list[Deck.Card]:
+        """Method to get the cards to pass from the player. This method will be called for each player at the beginning of the round."""
+        player_socket = self.players[player.name]
+        player_socket.send(Game.printer(
+            f'Passing {self.api.get_passing_direction()}\n', color=self.printer.CYAN, bold=True, underline=True))
+        player_state = self.api.get_player_state(player)
+        player_hand = player_state['hand']
+
+        chosen_cards = []
+        for i in range(3):
+
+            player_socket.send(
+                Game.printer("Your hand:", color=Game.printer.HEADER))
+            player_socket.send(
+                Game.printer.display_hand(player_hand))
+
+            card_index = self.get_valid_user_input(
+                player_socket, "Enter the card to pass by number in list: ", len(
+                    player_hand)
+            )
+            chosen_cards.append(player_hand[card_index])
+            player_hand.pop(card_index)
+        player_socket.send(Game.printer.clear())
+        return chosen_cards
+
+    def trick_end_hook(self, trick: 'Round.Trick') -> None:
+        trick_outcome = str(trick)
+        for player in self.players:
+            self.players[player].send(Game.printer.clear())
+            self.players[player].send(
+                Game.printer(f"\n{'-'*10}\n\n{trick_outcome}\n\n{'-'*10}\n", color=Game.printer.CYAN))
+
+    def card_played_hook(self, player: Player, card: Deck.Card) -> None:
+        for p in self.players:
+            if p != player.name:
+                self.players[p].send(Game.printer(
+                    f"{player.name}: {card}", color=Game.printer.CYAN))
+
+    def hearts_broken_hook(self):
+        for player in self.players:
+            self.players[player].send(Game.printer(
+                "\nHearts has been broken!\n", bold=True, color=Game.printer.GREEN))
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Server shutting down...")
+        exit()
